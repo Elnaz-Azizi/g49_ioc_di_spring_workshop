37bcfe87d7923c466139f0f99701e146bef31326
Elnaz <azizi.elnaz66@gmail.com>
2024-05-22 11:08:52 +0200

    Implemented Student class, StudentDao, StudentDaoListImpl, StudentSequencer and StudentNotFoundException.

--- src/main/java/se/lexicon/data_access/StudentDaoListImpl.java
+++ src/main/java/se/lexicon/data_access/StudentDaoListImpl.java
@@ -2,7 +2,46 @@ package se.lexicon.data_access;
 
 
 import org.springframework.stereotype.Component;
+import se.lexicon.data_access.sequencer.StudentIdSequencer;
+import se.lexicon.exception.StudentNotFoundException;
+import se.lexicon.models.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Component
-public class StudentDaoListImpl implements StudentDao{
+public class StudentDaoListImpl implements StudentDao {
+
+    private List<Student> students;
+
+    public StudentDaoListImpl() {
+        students = new ArrayList<>();
+    }
+
+    @Override
+    public Student save(Student student) {
+        if (student == null) throw new IllegalArgumentException("Student is null");
+        student.setId(StudentIdSequencer.nextId());
+        students.add(student);
+        return student;
+    }
+
+    @Override
+    public Student find(int id) throws StudentNotFoundException {
+        return students.stream()
+                .filter(student -> student.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new StudentNotFoundException("Student not found"));
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return new ArrayList<>(students);
+    }
+
+    @Override
+    public void delete(int id) throws StudentNotFoundException {
+        Optional.ofNullable(find(id)).ifPresent(students::remove);
+    }
 }
