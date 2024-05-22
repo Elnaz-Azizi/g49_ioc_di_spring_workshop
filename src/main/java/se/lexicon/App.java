89ccbd4da1d05eced0d6fa1133d4cb276e4729b7
Elnaz <azizi.elnaz66@gmail.com>
2024-05-22 11:56:45 +0200

    Impemented util package with UserInputService interface and ScannerInputService

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -3,6 +3,7 @@ package se.lexicon;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import se.lexicon.config.ComponentScanConfig;
 import se.lexicon.data_access.StudentDao;
+import se.lexicon.util.UserInputService;
 
 /**
  * Hello world!
@@ -13,7 +14,9 @@ public class App
     public static void main( String[] args )
     {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ComponentScanConfig.class);
-        StudentDao studentDao = context.getBean(StudentDao.class);
+        //StudentDao studentDao = context.getBean(StudentDao.class);
+
+        UserInputService userInputService =context.getBean(UserInputService.class);
 
     }
 }
